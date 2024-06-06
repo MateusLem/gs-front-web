9b0fa5c24bfef0d1389d16a9567f3c8d99f41e95
David Gabriel Gomes Fernandes <davidbiel2013@gmail.com>
2024-06-06 00:34:27 -0300

    fix{mexemos no texto e concertamos o site}

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
+const menu = document.querySelector(".menu-lateral");
+const linksSections = [
+    {link: "linkProblema", section: "problema"},
+    {link: "linkTecnologia", section: "tecnologia"},
+    {link: "linkObjetivo", section: "objetivo"},
+    {link: "linkPublico", section: "publico"},
+    {link: "linkBeneficios", section: "beneficios"},
+    {link: "linkImpacto", section: "impacto"}
+];
+
+// Funções para abrir e fechar o menu lateral
 function abrirNav() {
-    document.getElementById("menuLateral").style.width = "15rem";
-    document.querySelector('nav').classList.add("nav-hidden");
+    const screenWidth = window.innerWidth;
+
+    // Condições para definir o font-size com base na largura da tela
+    if (screenWidth >= 1300) {
+        menu.style.width = "20%";
+
+    } else if (screenWidth >= 650) {
+        menu.style.width = "20%";
+        document.body.style.fontSize = "16px";
+
+    } else if (screenWidth >= 425) {
+        menu.style.width = "30%";
+        document.body.style.fontSize = "14px";
+
+    } else if (screenWidth >= 375) {
+        menu.style.width = "35%";
+        document.body.style.fontSize = "12px";
+
+    } else if (screenWidth >= 320) {
+        menu.style.width = "40%";
+        document.body.style.fontSize = "12px";
+
+    } else {
+        menu.style.width = "40%";
+        document.body.style.fontSize = "12px";
+    }
 }
 
 function fecharNav() {
-    document.getElementById("menuLateral").style.width = "0";
-    document.querySelector('nav').classList.remove("nav-hidden");
+    menu.style.width = "0";
 }
+
+
+// Função genérica para adicionar eventos de clique nos links
+function addScrollEvent(linkId, sectionId) {
+    document.getElementById(linkId).addEventListener("click", function() {
+        scrollToSection(sectionId);
+    });
+}
+
+// Função para rolar até a seção específica com deslocamento de cabeçalho
+function scrollToSection(id) {
+    const element = document.getElementById(id);
+    const headerOffset = 60;
+    const elementPosition = element.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollBy({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
+linksSections.forEach(item => addScrollEvent(item.link, item.section));
